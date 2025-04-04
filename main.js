ebb69e8a3564a0edc0f406098eee3455788ec9e3
King182008 <cte-xaiden.morey@sllboces.org>
2025-04-04 17:20:12 +0000

    Digity Done

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 })
+
+$("#progress-button1").click(function(){
+    $("#progress").animate({width: "10%"}, "slow")
+    $("#progress").animate({width: "100%"}, "slow")
+})
+
+$("#progress-button2").click(function(){
+    $("#progress2").animate({width: "5%"}, "slow", function(){
+        $("#progress2").animate({width: "10%"}, "slow")
+        $("#progress2").animate({width: "20%"}, "fast", function(){
+            $("#progress2").animate({width: "50%"}, "slow")
+                $("#progress2").animate({width: "52%"}, "slow", function(){
+                    $("#progress2").animate({width: "55%"}, "slow")
+                    $("#progress2").animate({width: "75%"}, "slow", function(){
+                        $("#progress2").animate({width: "85%"}, "slow")
+                        $("#progress2").animate({width: "95%"}, "slow")
+                        $("#progress2").animate({width: "100%"}, "slow")
+                    })
+                })
+        })
+    })
+})
